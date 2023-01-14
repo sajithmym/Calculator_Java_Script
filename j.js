ce41bbbad48cbbe5e9db557bcaf233539bb4ef6f
Sajithmym <Sajithmym@gmail.com>
2023-01-14 22:24:37 +0530

    open commit

---
+++ j.js
@@ -0,0 +1,18 @@
+function Type(omg){
+    document.getElementById("inp").value += omg
+}
+
+function clean(){
+    document.getElementById("inp").value = ""
+}
+
+function Done(){
+    try{
+        var Get = document.getElementById("inp").value
+        var answer = eval(Get)
+        document.getElementById("inp").value = answer
+    }catch(e){
+        alert("Invaid Entry Check and Try Again...")
+    }
+}
+
